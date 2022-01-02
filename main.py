6a9e704f0e8d15ec32e1a862aa1d910d53a8db6a
Manab Kumar Biswas <77844663+Manab784@users.noreply.github.com>
2022-01-02 19:52:04 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,84 @@
+import numpy as np
+import tensorflow
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, BatchNormalization, Activation
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.constraints import maxnorm
+from keras.utils import np_utils
+import cv2
+from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
+import warnings
+warnings.filterwarnings('ignore')
+
+train_path = "C:/Users/Manab/Waste_Recognition_System/DATASET/TRAIN"
+test_path = "C:/Users/Manab/Waste_Recognition_System/DATASET/TEST"
+
+model = Sequential()
+
+model.add(Conv2D(32, (3, 3), input_shape=(224,224,3), padding='same'))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Conv2D(128, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Conv2D(256, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Flatten())
+model.add(Dropout(0.2))
+
+model.add(Dense(256, kernel_constraint=maxnorm(3)))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Dense(128, kernel_constraint=maxnorm(3)))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Dense(64, kernel_constraint=maxnorm(3)))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Dense(64, kernel_constraint=maxnorm(3)))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Dense(256))
+model.add(Activation('softmax'))
+
+optimizer = 'adam'
+
+model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+print(model.summary())
+
+train_data = ImageDataGenerator(rescale= 1./255)
+test_data = ImageDataGenerator(rescale= 1./255)
+train_generator = train_data.flow_from_directory(train_path,target_size= (224,224),batch_size = 256,color_mode= "rgb",class_mode= "categorical")
+test_generator = test_data.flow_from_directory(test_path,target_size= (224,224),batch_size = 256,color_mode= "rgb",class_mode= "categorical")
+
+model.fit_generator(train_generator, 25 ,test_generator)
+
+model.save('CNN_ImageProcessing_Manab.h5')
